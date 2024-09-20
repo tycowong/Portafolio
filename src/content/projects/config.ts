50b35112ec9dfe8a6a8818bd25dcaaf3e8788af0
Roberto Wong <33732733+tycowong@users.noreply.github.com>
2024-09-20 14:06:22 -0700

    cahnged project json file to separate md files

---
+++ src/content/projects/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from "astro:content";
+
+const projects = defineCollection({
+    schema: z.object({
+        name: z.string(),
+        img: z.string(),
+        sm_description: z.string(),
+        technologies: z.array(z.string())
+    })
+})
+
+export const collections = { projects }
